79d214c4bcf83526131e470066ce870ff5968f28
SP Singh <mannecheema@gmail.com>
2020-12-11 17:24:34 +1100

    Fixed failing tests

--- tests/Resource/OtpTest.php
+++ tests/Resource/OtpTest.php
@@ -349,7 +349,7 @@ class OtpTest extends TestCase
         try {
             $otp = new Otp();
             // failing with using non UTF8 text.
-            $otp->verfiyByRequestId('404372541682577504482079', utf8_decode("ü"));
+            $otp->verifyByRequestId('404372541682577504482079', utf8_decode("ü"));
         } catch (ResourceNotFoundException $e) {
             $this->fail('This test should not have failed');
         } catch (CredentialsException $e) {
